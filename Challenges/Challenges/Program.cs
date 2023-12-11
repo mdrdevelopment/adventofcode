cce8bd8b312b1d2bf7c46cec4a2ff27281f437e3
Cristian Medrea <cristi.medrea@microsec.co.uk>
2023-12-11 20:54:09 +0200

    Solve problem no. 2

--- Challenges/Challenges/Program.cs
+++ Challenges/Challenges/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace Challenges
 {
+    using System.Drawing;
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine($"Problem 1: {Trebuchet.Solve()}");
+            CubeConundrum.Solve(14,12,13);
         }
     }
 }
